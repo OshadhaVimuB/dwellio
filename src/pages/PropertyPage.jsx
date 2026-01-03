cbc88e6927dd73b82bd500dec170f84e223fbde8
OshadhaVimuB <oshadhavjb@gmail.com>
2026-01-03 19:44:22 +0530

    Fix errors in dependencies

---
+++ src/pages/PropertyPage.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const PropertyPage = () => <div><h1>Property Details</h1></div>;
+export default PropertyPage;
---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const SearchPage = () => <div><h1>Dwellio Search</h1></div>;
+export default SearchPage;
